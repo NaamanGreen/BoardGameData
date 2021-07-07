a0f16f95c37233eceaf6ae9159b9fd8b29a2cc4a
Naaman Green <naamangreen@brandeis.edu>
2021-07-07 18:43:40 -0400

    located bug when property is undefined

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './GameDetail.css';
 
 const GameDetail = ({game}) => {
     if(!game) {
--- src/components/GameList.js
+++ src/components/GameList.js
@@ -5,7 +5,7 @@ const GameList = ({games, onGameSelect}) => {
 
     const renderedList = games.map((game) => {
 
-        if(game.type==="game"  && game.rank < 9999999) {
+        if(game.type==="game" && game.rank < 9999999) {
             return <GameItem key={game.id} onGameSelect={onGameSelect} game={game} />;
         } 
     });
