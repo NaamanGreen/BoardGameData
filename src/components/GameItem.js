a5f3d3d309794d290d68dc9a4f7b171b509cc559
Naaman Green <naamangreen@brandeis.edu>
2021-07-08 14:58:57 -0400

    improved GameItem styling

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -15,16 +15,17 @@ const GameItem = ({game, onGameSelect}) => {
   
     return (
         <div onClick={() => onGameSelect(game)} className="game-item item">
-            <img alt={game.name} className="ui image" src={game.images.original} />
             <div className="content">
-                <div className="header">
-                    <p id="game-title">{game.name}</p>
-                </div>
+                <img alt={game.name} className="ui image" src={game.images.original} />
                 <div>
+                    <div className="header">
+                        <p id="game-title">{game.name}</p>
+                    </div>
                     <p>
-                        <strong>Publisher:</strong> {checkPublisher()} <br/>
                         <strong>Players:</strong> {game.min_players}-{game.max_players} <br/>
-                        <strong>Play time:</strong> {game.min_playtime}-{game.max_playtime} minutes
+                        <strong>Play time:</strong> {game.min_playtime}-{game.max_playtime} minutes <br/>
+                        <strong>Publisher:</strong> {checkPublisher()} <br/>
+                        <strong>Year Published:</strong> {game.year_published} <br/>
                     </p>
                 </div>
             </div>
